4ec518481febed98c8fefc468be8666f22a4eced
Sixten Paulsson <sixtenpaulsson@gmail.com>
2025-04-23 14:05:39 +0200

    sak

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/model/recipe_database/recipe_handler.dart';
 import 'package:lab2/pages/main_view.dart';
+import 'package:lab2/ui_controller.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => RecipeHandler()),
+      ChangeNotifierProvider(create: (context) => UIController()),
+      ],
+    child: const MyApp(),
+    ));
 }
 
 class MyApp extends StatelessWidget {
--- lab2/lib/pages/main_view.dart
+++ lab2/lib/pages/main_view.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/model/recipe_database/ingredient.dart';
+import 'package:lab2/util/cuisine.dart';
+import 'package:lab2/widgets/difficulty_control.dart';
+import 'package:lab2/widgets/ingredient_control.dart';
+import 'package:lab2/widgets/kitchen_control.dart';
+import 'package:lab2/widgets/price_control.dart';
+import 'package:lab2/widgets/recipe_area.dart';
+import 'package:lab2/widgets/recipe_detail.dart';
+import 'package:lab2/widgets/recipe_list.dart';
+import 'package:lab2/widgets/time_control.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder());
-  }
+@override
+Widget build(BuildContext context) {
+   return Scaffold(
+     body: Row(children: [_controlPanel(context), RecipeArea()]),
+   );
+}
+
+Widget _controlPanel(context, {double width = 320}) {
+   return Container(
+    width: width,
+    color: const Color.fromARGB(255, 193, 210, 218),
+    child: Column(
+      children: [
+        Text('Receptsök'),
+        Text('Sök efter recept'),
+        Row(
+          children: [
+            Text("Ingrediens"),
+            IngredientControl()
+          ],),
+        Row(
+          children: [
+            Text("Kök"),
+            KitchenControl()
+          ],
+        ),
+
+        Text("Svårighet"),
+        DifficulyControl(),
+        Row(
+          children: [
+            Text("Pris"),
+            PriceControl()
+          ],
+        ),
+        Row(
+          children: [
+            Text("Tid"),
+            TimeControl()
+          ],
+        ),
+
+        ],)
+    );
+}
+
+
+
+
 }
