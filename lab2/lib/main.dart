ca0693a1c71b4f79bf47f9688e7c45a7f558a766
viktorwaller <wallerv@chalmers.se>
2025-05-06 12:38:17 +0200

    klar

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:lab2/pages/main_view.dart';
 import 'package:lab2/ui_controller.dart';
 import 'package:provider/provider.dart';
 
+
+
 void main() {
   runApp(
     MultiProvider(providers: [
